fa69a29b5e71b3d9957fe3bcb01cf79636de4ebf
Yeuoly <admin@srmxy.cn>
2024-06-30 19:49:22 +0800

    feat: qwen2 and gemma

---
+++ models/gemma.py
@@ -0,0 +1,47 @@
+import os
+
+from openai import OpenAI
+
+from needle_not_in_a_haystack import NeedleNotInAHaystack
+
+DEEPINFRA_API_KEY = os.environ.get("DEEPINFRA_API_KEY")
+
+class Qwen272BNeedleNotInAHaystack(NeedleNotInAHaystack):
+    model_name = 'google/gemma-1.1-7b-it'
+    max_batch_size = 1024 * 1
+
+    def llm(self, corpus: str, query: str) -> str:
+        """
+        Implement LLM invocation here.
+        """
+        client = OpenAI(api_key=DEEPINFRA_API_KEY, base_url='https://api.deepinfra.com/v1/openai')
+        response = client.chat.completions.create(
+            model=self.model_name,
+            messages=[
+                {
+                    'role': 'system',
+                    'content': 'You have lots of number, and each of them has a unique answer. You need to find the answer of a number.\n'
+                        'Below are the numbers and their answers:\n' + corpus
+                },
+                {
+                    'role': 'user',
+                    'content': query
+                }
+            ],
+            stream=False,
+            temperature=0
+        )
+
+        text = response.choices[0].message.content
+
+        return text
+    
+    def check_answer(self, response: str, needle: str) -> bool:
+        """
+        Return True if the response contains the needle, False otherwise
+        """
+        response = response.lower()
+        if 'not' in response or 'sorry' in response or 'don\'t' in response or 'haven\'t' in response:
+            return False
+        return True
+    
